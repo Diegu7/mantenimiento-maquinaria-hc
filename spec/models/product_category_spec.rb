b4c1ccdbf975fbcfc4772068769673879691d520
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-05-22 21:36:32 -0600

    Specs for machine sections, product brands and categories added

--- spec/models/product_category_spec.rb
+++ spec/models/product_category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ProductCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_product_category) { build :product_category }
+  let(:product_category_without_name) { build :product_category, name: nil}
+
+  it "is valid with name" do
+    expect(valid_product_category).to be_valid
+  end
+  
+  it "is invalid without a name" do
+    product_category_without_name.valid?
+    expect(product_category_without_name.errors[:name]).to include("can't be blank")
+  end  
 end
