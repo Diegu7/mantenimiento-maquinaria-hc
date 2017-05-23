2982317250847854eece18d36ab3576d39fadc5c
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-05-22 22:02:33 -0600

    Specs for mileage logs and its factories file

---
+++ spec/factories/mileage_logs.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :mileage_log do
+        date { Time.now }
+        hours 20
+        machine
+    end
+end
+ 
--- spec/models/mileage_log_spec.rb
+++ spec/models/mileage_log_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe MileageLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_mileage_log) { build :mileage_log }
+  let(:mileage_without_date) { build :mileage_log, date: nil}
+  let(:mileage_without_valid_hours) { build :mileage_log, hours: -1 }
+  let(:mileage_without_valid_hours_0) { build :mileage_log, hours: 0 }
+  let(:mileage_without_machine) { build :mileage_log, machine: nil}
+
+  it "is valid with a date, hours and a machine" do
+    expect(valid_mileage_log).to be_valid
+  end
+  
+  it "is invalid without a date" do
+    mileage_without_date.valid?
+    expect(mileage_without_date.errors[:date]).to include("can't be blank")
+  end
+  
+  it "is invalid with an hours value less than 0" do
+    mileage_without_valid_hours.valid?
+    expect(mileage_without_valid_hours.errors[:hours]).to include("must be greater than 0")
+  end
+
+  it "is invalid with an hours value less than 0" do
+    mileage_without_valid_hours_0.valid?
+    expect(mileage_without_valid_hours_0.errors[:hours]).to include("must be greater than 0")
+  end
+
+  it "is invalid with a machine" do
+    mileage_without_machine.valid?
+    expect(mileage_without_machine.errors[:machine]).to include("can't be blank")
+  end  
+    
 end
