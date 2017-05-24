6268c74ea61720f4b0b9f17fb691871bdafcbb6c
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-05-23 19:58:50 -0600

    Technical specifications model tests completed

--- spec/models/technical_specification_spec.rb
+++ spec/models/technical_specification_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe TechnicalSpecification, type: :model do
     expect(:valid_technical_specification).to be_valid
   end
 
+  it "is invalid without a key" do
+    specification_without_key.valid?
+    expect(specification_without_key.errors[:key]).to include("can't be blank")
+  end
+  
+  it "is invalid without a value" do
+    specification_without_value.valid?
+    expect(specification_without_value.errors[:value]).to include("can't be blank")
+  end
+  
+  it "is invalid without a machine" do
+    specification_without_machine.valid?
+    expect(specification_without_machine.errors[:machine]).to include("can't be blank")
+  end
 end
