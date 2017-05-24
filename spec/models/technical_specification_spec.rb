79d88d7a5d22e588c600661b200385c18f2f47d2
jimmybanegas93 <jimmy.ramos@unitec.edu>
2017-05-23 19:43:42 -0600

    Technical specifications specs

--- spec/models/technical_specification_spec.rb
+++ spec/models/technical_specification_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TechnicalSpecification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_technical_specification) { build :technical_specification }
+  let(:specification_without_key) { build :technical_specification, key: nil }
+  let(:specification_without_value) { build :technical_specification, value: nil }
+  let(:specification_without_machine) { build :technical_specification, machine: nil }
+
+  it "is valid with key, value and machine" do
+    expect(:valid_technical_specification).to be_valid
+  end
+
 end
