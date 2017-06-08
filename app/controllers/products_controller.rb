8c5bf2173f70a5513708a6ca21e7d868a9e0afff
Jose Rodriguez <jose.shamm@gmail.com>
2017-06-08 17:14:07 -0600

    Pushing to test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,29 +8,48 @@ class ProductsController < ApplicationController
             @products = brand.products.order(:name)
         end
     end
-  end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-
-    if @product.update_attributes(product_params)
-      redirect_to @product
-    else
-      render :edit
+    def show
+        @product = Product.find(params[:id])
     end
-  end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to products_path
-  end
-  protected
-    def product_params
-        params.require(:product).permit(:name, :image, :product_brand_id, :initial_stock, :current_stock, :minimum, :maximum, :specifications)
+    def new
+        @product = Product.new
     end
+
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            redirect_to @product
+        else
+            flash[:errors] = "No se pudo registrar el producto"
+            render :new
+        end
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+
+        if @product.update_attributes(product_params)
+            redirect_to @product
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect_to products_path
+    end
+
+    protected
+        def product_params
+            params.require(:product).permit(:name, :image, :product_brand_id, :initial_stock, :current_stock, :minimum, :maximum, :specifications)
+        end
 end
