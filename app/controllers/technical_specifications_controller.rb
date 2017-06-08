d1163e7f92f8d2047dc7034f8f902f410350822a
Jose Rodriguez <jose.shamm@gmail.com>
2017-06-08 03:13:04 -0600

    Can now make 6 technical specifications at a time

--- app/controllers/technical_specifications_controller.rb
+++ app/controllers/technical_specifications_controller.rb
@@ -5,24 +5,41 @@ class TechnicalSpecificationsController < ApplicationController
 
   def new
     @machine= Machine.find(params[:machine_id])
-    @datasheet= @machine.technical_specifications.new
+    @datasheet=[]
+    @datasheet << @machine.technical_specifications.new
+    @datasheet << @machine.technical_specifications.new
+    @datasheet << @machine.technical_specifications.new
+    @datasheet << @machine.technical_specifications.new
+    @datasheet << @machine.technical_specifications.new
+    @datasheet << @machine.technical_specifications.new
+    #@datasheet= @machine.technical_specifications.new
   end
 
   def create
     @machine= Machine.find(params[:machine_id])
-    @datasheet = @machine.technical_specifications.new(specification_params)
-
-    if @datasheet.save
-      redirect_to @machine
+    if params.has_key?("technical_specification")
+      tempTech=@machine.technical_specifications.new(specification_params("technical_specification"))
+      tempTech.save
     else
-      flash[:errors] = "No se pudo registrar la maquina"
-      render :new
+      for specification in params["technical_specifications"] do
+        if specification["key"] != "" || specification["value"] != ""
+          tempTech=@machine.technical_specifications.new(specification_params(specification))
+          tempTech.save
+        end
+      end
     end
+    redirect_to @machine
+#    if @datasheet.save
+#      redirect_to @machine
+#    else
+#      flash[:errors] = "No se pudo registrar la maquina"
+#      render :new
+#    end
   end
 
   protected
-    def specification_params
-      params.require(:technical_specification).permit(:key, :value, :machine_id)
+    def specification_params(my_params)
+      my_params.permit(:key, :value, :machine_id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post '/machines/:machine_id/technical_specifications/new', to: 'technical_specifications#create' 
 
   resources :machines do
     resources :technical_specifications
