0441dc8c2efa4a395fbdb7b9c3694d7ed8b1a440
AstroThing <andres.rodriguezhn@gmail.com>
2017-06-07 21:30:16 -0600

    Added total_hours method to Machine

--- app/models/mileage_log.rb
+++ app/models/mileage_log.rb
@@ -1,7 +1,7 @@
 class MileageLog < ApplicationRecord
-     belongs_to :machine
-     
-     validates_presence_of :date
-     validates_presence_of :hours
-     validates :hours, numericality: { greater_than: 0 }
+    belongs_to :machine
+
+    validates_presence_of :date
+    validates_presence_of :hours
+    validates :hours, numericality: { greater_than: 0 }
 end
