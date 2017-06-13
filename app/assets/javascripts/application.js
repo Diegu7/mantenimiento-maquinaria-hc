77971cb21666cf823f665b404e3d3ee4dbec6ae1
Nimer <nimerabraham@gmail.com>
2017-06-13 12:54:10 -0600

    Added Search-bar and Pagination

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,9 @@
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/extras/dataTables.buttons
 //= require_tree .
 //= require bootstrap-sprockets
 //= require bootstrap-modal
