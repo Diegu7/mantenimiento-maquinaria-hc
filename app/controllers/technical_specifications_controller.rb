42d96a608f651084c8dcc0a8702871d016507a8a
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-06-16 20:53:47 -0600

    working on controller

--- app/controllers/technical_specifications_controller.rb
+++ app/controllers/technical_specifications_controller.rb
@@ -13,22 +13,42 @@ class TechnicalSpecificationsController < ApplicationController
 
   def update
     @machine= Machine.find(params[:machine_id])
-    @specifications = @machine.technical_specifications
+    #@specifications = params[:machine][:technical_specifications_attributes]
 
-    if @machine.save
-      redirect_to @machine
-    else
-       flash[:errors] = "No se pudo crear la transacción"
-       render :new
+    params[:machine][:technical_specifications_attributes].each do |k, v|
+      key  = v["key"]
+      value = v["value"]
+      
+      @specification = TechnicalSpecification.new(key: key, value: value, machine_id: params[:machine_id])
+      tempTech=@machine.technical_specifications.new(specification_params(@specification))
+      tempTech.save
     end
+
+    redirect_to @machine
+
+    # for specification in @specifications do
+    #   if specification["key"] != "" || specification["value"] != ""
+    #     tempTech=@machine.technical_specifications.new(specification_params(specification))
+    #     tempTech.save
+    #   end
+
+    #   redirect_to @machine
+    # end
+
+    # flash[:errors] = "No se pudo crear la transacción"
+    # render :new
   end
   
   def destroy 
   end
   
   protected
-    def specification_params(my_params)
+    def specification_params2(my_params)
       my_params.permit(technical_specifications_attributes [:key, :value])
+    end
+
+    def specification_params(my_params)
+      params.require(:technical_specification).permit(:key, :value, :machine_id)
     end
 
 end
