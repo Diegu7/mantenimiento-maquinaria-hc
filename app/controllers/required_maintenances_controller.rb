621c78cb595a9c9cf78f12f41d0090d843468e61
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-06-21 01:07:39 -0600

    Basic create required maintenance

--- app/controllers/required_maintenances_controller.rb
+++ app/controllers/required_maintenances_controller.rb
@@ -1,6 +1,5 @@
 class RequiredMaintenancesController < ApplicationController
     def index
-        # @machine = Machine.find(params[:machine_id])
     end
 
     def new
@@ -13,7 +12,7 @@ class RequiredMaintenancesController < ApplicationController
         @required_maintenance = @machine.required_maintenances.build(required_maintenance_params)
 
         if @required_maintenance.save
-            redirect_to machine_required_maintenances_path(@machine)
+            redirect_to @machine
         else
             flash[:errors] = "No se pudo crear el mantenimiento requerido"
             render :new
@@ -24,11 +23,11 @@ class RequiredMaintenancesController < ApplicationController
         @machine = Machine.find(params[:machine_id])
         @required_maintenance = @machine.required_maintenances.find(params[:id])
         @required_maintenance.destroy
-        redirect_to machine_required_maintenances_path(@machine)
+        redirect_to @machine
     end
 
     protected
         def required_maintenance_params
-            params.require(:required_maintenance).permit(:description, :estimated_duration, :machine_id, :machine_area_id)
+            params.require(:required_maintenance).permit(:description, :estimated_duration, :machine_id, :machine_area_id, :frequency_in_hours, :frequency_in_days, :last_time_done_at, :mileage_when_last_done)
         end
 end
--- app/models/required_maintenance.rb
+++ app/models/required_maintenance.rb
@@ -1,9 +1,26 @@
 class RequiredMaintenance < ApplicationRecord
+     before_save :fill_nil_values
      belongs_to :machine
-     
+     belongs_to :machine_area
+
      has_many :programmed_maintenances
 
-     validates_presence_of :description, :machine, :machine_area, :estimated_duration, :frequency_in_hours, :frequency_in_days
+     validates_presence_of :description, :machine, :machine_area, :frequency_in_hours, :frequency_in_days
 
-     validates :estimated_duration, :frequency_in_hours, :frequency_in_days, numericality:  { greater_than_or_equal_to: 0 }
+     validates :estimated_duration, :mileage_when_last_done, :frequency_in_hours, :frequency_in_days, numericality: { greater_or_equal_than: 0 }     
+     
+     def fill_nil_values
+        if self.frequency_in_days == nil
+             self.frequency_in_days = 0
+        end
+
+        if self.frequency_in_hours == nil
+             self.frequency_in_hours = 0
+        end
+
+        if self.mileage_when_last_done == nil
+             self.mileage_when_last_done = 0
+        end
+     end
+        
 end
