dee2bcdbe651714ae6a28a4c52c800a6f405bce2
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-06-20 23:17:41 -0600

    Clean mileage log controller

--- app/controllers/mileage_logs_controller.rb
+++ app/controllers/mileage_logs_controller.rb
@@ -13,36 +13,6 @@ class MileageLogsController < ApplicationController
     @mileage_log = @machine.mileage_logs.build(mileage_log_params)
 
     if @mileage_log.save
-
-      #  @required_maintenances = @machine.required_maintenances
-        
-      #   @required_maintenances.each  do |maintenance|
-      #       if maintenance.frequency_in_hours.present?
-      #           @mileage_when_last_done = maintenance.mileage_when_last_done
-      #           @current_mileage = @machine.total_hours
-      #           @maintenance = RequiredMaintenance.find(maintenance.id)
-
-      #           if @current_mileage - @mileage_when_last_done >= maintenance.frequency_in_hours
-      #               # Validate if already exists a programmed maintenance for this required maintenance in this machine
-      #               puts "LLEGA HASTA ANTES DEL IF"
-      #               #if !(ProgrammedMaintenance.exists?(required_maintenance_id: maintenance.id))
-      #                   # puts "SEGUNDO IF"                        
-      #                   programmed = ProgrammedMaintenance.new
-      #                   programmed.description = maintenance.description
-      #                   programmed.scheduled_at = Date.today
-      #                   programmed.estimated_duration = maintenance.estimated_duration
-      #                   programmed.comments = "."
-      #                   programmed.done_at = 1.day.from_now
-      #                   programmed.done ||= false
-      #                   programmed.preventive ||= false
-      #                   programmed.machine = @machine
-      #                   programmed.required_maintenance_id = @maintenance.id
-      #                   programmed.save!
-      #               #end
-      #           end
-      #       end
-      #   end
-
       redirect_to machine_mileage_logs_path(@machine)
     else
       flash[:errors] = "No se pudo ingresar el horometraje"
