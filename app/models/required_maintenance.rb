4cd44cf8fcd4786d76415d21e414a31f718143bc
AstroThing <andres.rodriguezhn@gmail.com>
2017-06-21 14:52:57 -0600

    Added function to get maintenances that are coming soon

--- app/models/required_maintenance.rb
+++ app/models/required_maintenance.rb
@@ -8,7 +8,12 @@ class RequiredMaintenance < ApplicationRecord
      validates_presence_of :description, :machine, :machine_area, :frequency_in_hours, :frequency_in_days
 
      validates :estimated_duration, :mileage_when_last_done, :frequency_in_hours, :frequency_in_days, numericality: { greater_or_equal_than: 0 }     
-     
+
+     #This should be a query
+     def self.coming_soon
+         RequiredMaintenance.all.select{ |m| m.last_time_done_at + m.frequency_in_days < Date.today + 5.day  }
+     end
+
      def fill_nil_values
         if self.frequency_in_days == nil
              self.frequency_in_days = 0
