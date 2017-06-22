4ab32449c5e25ffb8058e3f7126c6bd4a46e7b73
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-06-21 23:27:58 -0600

    Creating maintenance plans with programmed maintenances

--- app/controllers/maintenance_plans_controller.rb
+++ app/controllers/maintenance_plans_controller.rb
@@ -5,7 +5,7 @@ class MaintenancePlansController < ApplicationController
 
     def new
         @maintenance_plan = MaintenancePlan.new
-        @programmed_maintenances = ProgrammedMaintenance.where(done: false, preventive: true)
+        @programmed_maintenances = ProgrammedMaintenance.where(done: false, preventive: true, scheduled: false)
     end
 
     def show
@@ -16,6 +16,12 @@ class MaintenancePlansController < ApplicationController
         @maintenance_plan = MaintenancePlan.new(plan_params)
 
         if @maintenance_plan.save
+            @programmed_maintenances = @maintenance_plan.programmed_maintenances
+
+            @programmed_maintenances.each do |maintenance|
+                maintenance.scheduled = true
+                maintenance.save!
+            end
             redirect_to @maintenance_plan
         else
             flash[:errors] = 'No se pudo crear el plan de mantemiento'
@@ -29,7 +35,14 @@ class MaintenancePlansController < ApplicationController
 
     def destroy
         @maintenance_plan = MaintenancePlan.find(params[:id])
-        @maintenance_plan.destroy
+        @programmed_maintenances = @maintenance_plan.programmed_maintenances      
+
+        if @maintenance_plan.destroy
+            @programmed_maintenances.each do |maintenance|
+                maintenance.scheduled.toggle!
+            end
+        end
+        
         redirect_to maintenance_plans_path
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622034250) do
+ActiveRecord::Schema.define(version: 20170622045124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20170622034250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "required_maintenance_id"
+    t.boolean "scheduled", default: false
     t.index ["machine_id"], name: "index_programmed_maintenances_on_machine_id"
     t.index ["required_maintenance_id"], name: "index_programmed_maintenances_on_required_maintenance_id"
   end
