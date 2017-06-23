31eba123822b159dbfd86c387208db08c0588968
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-06-23 12:46:06 -0600

    mark maintenance plan as done

--- app/controllers/programmed_maintenances_controller.rb
+++ app/controllers/programmed_maintenances_controller.rb
@@ -1,4 +1,8 @@
 class ProgrammedMaintenancesController < ApplicationController
+    def index
+        @programmed_maintenances = ProgrammedMaintenance.where(preventive: true, done: false).order(:scheduled_at)
+    end
+    
     def new_corrective
         @programmed_maintenance = ProgrammedMaintenance.new
 		@programmed_maintenance.materials_for_maintenances.build
@@ -18,6 +22,42 @@ class ProgrammedMaintenancesController < ApplicationController
             flash[:errors] = "No se pudo crear el mantenimiento"
             render :new_corrective
         end
+    end
+
+    def show
+        @programmed_maintenance = ProgrammedMaintenance.find(params[:id])
+    end
+
+    def edit
+        @programmed_maintenance = ProgrammedMaintenance.find(params[:id])
+    end
+
+    def update
+        @programmed_maintenance = ProgrammedMaintenance.find(params[:id])
+        @products = Product.all.order(:name)
+        @machines = Machine.all.order(:name)
+        
+        @maintenance_plan =  @programmed_maintenance.maintenance_plans.first
+
+        @programmed_maintenance.done = true
+
+        if @programmed_maintenance.update_attributes(corrective_maintenance_params)
+            redirect_to @maintenance_plan
+        else
+            render :template => "maintenance_plans/done"
+        end
+    end
+
+    def destroy       
+        @programmed_maintenance = ProgrammedMaintenance.find(params[:id])
+
+        if @programmed_maintenance.maintenance_plans.blank?
+            @programmed_maintenance.destroy
+            redirect_to programmed_maintenances_path
+        else
+            flash[:errors] = "No se puede borrar porque pertenece a un plan, borre el plan primero"
+            redirect_to programmed_maintenances_path
+        end           
     end
 
     def events
