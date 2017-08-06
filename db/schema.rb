3e6c48670be3d0bbd4ebe9238734538fde1465b2
Derick Noel Arzú Morales <arzuderick@unitec.edu>
2017-08-06 02:51:26 -0600

    Subir Imagen Productos y Maquinas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806082314) do
+ActiveRecord::Schema.define(version: 20170806084526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,13 +100,16 @@ ActiveRecord::Schema.define(version: 20170806082314) do
     t.string "name"
     t.integer "initial_stock"
     t.integer "current_stock"
-    t.string "image"
     t.bigint "product_brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "minimum"
     t.integer "maximum"
     t.string "specifications"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["product_brand_id"], name: "index_products_on_product_brand_id"
   end
 
