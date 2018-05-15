279dd93aaacee3208465c9b7174c90293b1583a6
roberto-627 <roberto.melara@unitec.edu>
2018-05-15 20:24:10 +0000

    .

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20170808213451) do
   create_table "maintenance_notifications", force: :cascade do |t|
     t.string "machine_name"
     t.text "description"
-    t.date "scheduled_at"
+    t.date "schedule_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "programmed_maintenance_id"
